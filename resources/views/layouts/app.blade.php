a4412cdc5ea07ddb1ed07778cb392b8935d3948c
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2024-10-19 15:26:48 +0100

    tuto_2

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Laravel App</title>
+</head>
+<body>
+    <header>
+        <h1>My site</h1>
+        <nav></nav>
+    </header>
+
+    @yield('content')
+
+    <footer>
+        &copy; 2024 all the right reserved
+    </footer>
+</body>
+</html>
