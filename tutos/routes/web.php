49e7cecc41ab1bdd8cf34f5c8fe27f599e1d5fbb
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2024-10-23 09:54:06 +0100

    Revert "Merge pull request #6 from Suirita/Tuto-3"
    This reverts commit 7d39ceb2fcaac9590a1d546984ee7a9d893722af, reversing
    changes made to fa5b1063e6fdd16933b25bfae401d79f030eb72c.

--- tutos/routes/web.php
+++ tutos/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,6 +9,3 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
-
-Route::get('/contact', [ContactController::class, 'create']);
-Route::post('/contact', [ContactController::class, 'store']);
